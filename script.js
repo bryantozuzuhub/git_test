5eb3da1efe077bab8292fb4466d772151013235a
Bryant Ozuzu <bryantozuzucontact@gmail.com>
2025-05-27 18:47:11 -0600

    i forgot to commit additions

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
+function seriesCall(any) {
+    alert("this is the result");
+}
 
+
+const circlepie = Math.PI;
+let circleRadius = 5.15;
+let circleArea = circlepie * circleRadius^2;
+console.log(`The area is ${circleArea}`);
 
